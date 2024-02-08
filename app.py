f473acb3dbbb75a2099ded3c0b95218ffd9e32d9
rssauri <rosemariesauri@loyalistcollege.com>
2024-02-07 22:16:34 -0500

    v1.23

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def add_member():
 
         return redirect(url_for('index'))
 
-@app.route('/delete_member', methods=['DELETE'])
+@app.route('/delete_member', methods=['POST'])
 def delete_member(id):
     clodmember = ClodMember.query.where(ClodMember.id == id).first()
     db.session.delete(clodmember)
